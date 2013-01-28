ce344e47af2fb6456e3ab7feeac50646fa4fe7b1
Konstantin Raev <bestander@gmail.com>
2013-01-29 00:40:11 +1300

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 jasmine-node-sugar
 ==================
 
-Some additional methods that make my life easier when testing with Jasmine in Node.js
+Some additional methods that make my life easier when testing with Jasmine (1.0.28) in Node.js  
+I decided not to push this one to npm while there are only a few insignificant features which probably will be fixed in the next Jasmine release.
+
+### Usage
+
+- add this repo as dependency in `package.json`:
+ 
+```javascript
+  "devDependencies": {
+    "jasmine-node": "1.0.28",
+    "jasmine-node-sugar": "git://github.com/bestander/jasmine-node-sugar.git#master"
+  },
+```
+
+- require this module in your `spec` file
+- call `jasmine-node-sugar.addJasmineMatchers` in `beforeEach` method of your suite/spec
+
+
+### License MIT
---
+++ index.js
@@ -0,0 +1,37 @@
+// timeout fix for Node.js
+jasmine.getGlobal().setTimeout = function(funcToCall, millis) {
+  if (jasmine.Clock.installed.setTimeout.apply) {
+    return jasmine.Clock.installed.setTimeout.apply(this, arguments);
+  } else {
+    return jasmine.Clock.installed.setTimeout(funcToCall, millis);
+  }
+};
+
+// TODO add other time-related Clock mocks: setInterval etc 
+
+module.exports = {
+
+  /**
+   * call this in beforeEach method to add more available matchers
+   * @param spec "this" within spec
+   */
+  addJasmineMatchers: function(spec) {
+    spec.addMatchers({
+      /**
+       * Array contains all elements of another array
+       * @param needles another array
+       * @returns {Boolean} true/false
+       */
+      toContainAll: function(needles) {
+        var haystack = this.actual;
+        return needles.every(function (elem) {
+          return haystack.some(function (sackElem) {
+            return spec.env.equals_(elem, sackElem);
+          });
+        });
+      }
+    });
+  }
+  
+};
+
